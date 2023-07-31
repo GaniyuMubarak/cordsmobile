10f24dbe525d15e5a2660f6ba2ac02150eff82e5
Ganiyu Mubarak <ganiyumubarak12@gmail.com>
2023-07-31 18:36:52 +0100

    database

--- database/cordsmobile.sql
+++ database/cordsmobile.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2023 at 02:47 PM
+-- Generation Time: Jul 31, 2023 at 07:35 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,12 +29,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `complaints` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `location` varchar(200) NOT NULL,
+  `userID` varchar(200) NOT NULL,
   `message` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `complaints`
+--
+
+INSERT INTO `complaints` (`id`, `userID`, `message`) VALUES
+(1, 'ganiyumubarak12@gmail.com', 'Good sir, we\'ve been facing slkaldjjaldjfjldfj');
 
 -- --------------------------------------------------------
 
@@ -44,10 +48,53 @@ CREATE TABLE `complaints` (
 
 CREATE TABLE `observation` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `location` varchar(200) NOT NULL,
-  `observation_message` longtext NOT NULL
+  `userID` varchar(200) NOT NULL,
+  `message` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `observation`
+--
+
+INSERT INTO `observation` (`id`, `userID`, `message`) VALUES
+(1, 'ganiyumubarak12@gmail.com', 'We observe that; the only place where light has been given since monday till now is Olonkoro.'),
+(2, 'ganiyumubarak12@gmail.com', 'Hello, we oljdjdsjjfd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `transformer_id` int(100) NOT NULL,
+  `message` longtext NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transformer`
+--
+
+CREATE TABLE `transformer` (
+  `id` int(11) NOT NULL,
+  `transformer_name` varchar(200) NOT NULL,
+  `longtitude` varchar(50) NOT NULL,
+  `latitude` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transformer`
+--
+
+INSERT INTO `transformer` (`id`, `transformer_name`, `longtitude`, `latitude`, `created_at`, `updated_at`) VALUES
+(1, 'Come to our Area for Clearification', '', '', '2023-05-10 14:40:54', '2023-05-10 14:41:16');
 
 -- --------------------------------------------------------
 
@@ -61,6 +108,7 @@ CREATE TABLE `users` (
   `email` varchar(200) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `location` varchar(200) DEFAULT NULL,
+  `password` varchar(200) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -69,9 +117,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `phone`, `location`, `created_at`, `updated_at`) VALUES
-(17, 'Ganiyu M.A.', 'ganiyumubarak12@gmail.com', '09073240313', 'Come', '2023-02-19 15:03:11', '2023-02-19 15:03:11'),
-(18, 'Ganiyu M.A.', 'ganiyumubarak12@gmail.com', '07059174722', 'Come', '2023-02-19 15:04:21', '2023-02-19 15:04:21');
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `location`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Ganiyu Mubarak A', 'ganiyumubarak12@gmail.com', '09073240313', 'Come', '$2y$10$K.aEBnrNTH.Benvv/gF7VeSVH4R5QY78nEkRPKI0vXQ/P9v6wqrv.', '2023-05-16 16:17:49', '2023-05-16 16:17:49'),
+(2, 'Hassan Muaz', 'hassanatalao@gmail.com', '07059174722', 'Go', '$2y$10$dSggPM73IHRhh.VI7C5hw.jxMpffbrbXSMQW68PMBXjqP3umkJmSC', '2023-07-31 17:11:05', '2023-07-31 17:11:05');
 
 --
 -- Indexes for dumped tables
@@ -90,6 +138,18 @@ ALTER TABLE `observation`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transformer`
+--
+ALTER TABLE `transformer`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -103,19 +163,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `complaints`
 --
 ALTER TABLE `complaints`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `observation`
 --
 ALTER TABLE `observation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transformer`
+--
+ALTER TABLE `transformer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
